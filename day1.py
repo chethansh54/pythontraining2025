a899f9d2bd821f3675566ddf80ece66294448516
chethansh54 <trainerchetanh@gmail.com>
2025-12-29 15:17:22 +0530

    variables

--- day1.py
+++ day1.py
@@ -1,5 +1,32 @@
-# Simple Program
-distance=10.0
-time=5.0
+# Python Data Types & Variables
 
-print(f"Speed = {distance/time}")
+# Number - integers, float (decimal numbers or fractions)
+# String - is a character or collection of characters, basically used for text data
+# Collections - kind of data types which are collections or group of other data types
+# - List - like array, collection of other data types, denoted by []
+# - Tuple - like array, collection of other data types, denoted by ()
+#         - Special kind of list, in which modification of data or elements is not allowed  
+# - Set - like list only, but duplicates are not allowed , denoted by {}
+# - Dictionary - special type of collection, which uses mapping of key and values
+#              - works like key : value pair
+#              - "class" : "Xth class"
+#              - denoted by { "key" : "value" , "key2" : "value"}
+
+
+distance=10
+name="gaurav"
+
+print("Distance ",distance," name = ", name)
+print(f"Distance = {distance}, name = {name}")
+
+fruits=["apples", "mango", "guava"]
+
+data=[10," hello", -10, 5.0 ]
+
+print(f"fruits list = {fruits}")
+print(f"data list = {data}")
+
+names={"gaurav", "gaurav", "daya","chetan"}
+
+print(f"names = {names}")
+
